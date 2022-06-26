a7f006725440542fdbe6c38202837a10489265b6
Victor Melo <victorvmelo@gmail.com>
2022-06-26 19:17:42 -0300

    Alarme

---
+++ src/Alarme.py
@@ -0,0 +1,16 @@
+class Alarme():
+    def __init__(self):
+        self.identificouUmFocoDeIncendio = False
+
+    def getIdentificouUmFocoDeIncendio(self):
+        return self.identificouUmFocoDeIncendio
+
+    def alertarAlertaParaAreaCental():
+        print("Alertar Area Central! Incendio Detectado!!")
+
+    def ligarParaEmergencia():
+        print("Esta é uma zona critica e não deve passar pela centra. Ligando para emergência imediatamente!!")
+
+    def ligarIndicadoresLuminosos():
+        print("Ligando indicadores luminosos")
+
